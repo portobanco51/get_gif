2dd115b1fcf86a4b041bb6c2ca78228bc9e7641e
Marcelo <76289550+portobanco51@users.noreply.github.com>
2024-11-15 02:35:17 -0600

    FIX: API key removal
    * Updated libraries and dependencies
    * Removed Api key

--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,16 +1,15 @@
-
 export const getGifs = async (cat) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(cat)}&limit=20&api_key=ixyF6yDFLPAzBe0fMQHFbbSsbRFzNH55`
-    const res = await fetch(url)
-    const { data } = await res.json()
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(cat)}&limit=20&api_key=${process.env.REACT_APP_API_KEY}`;
+    const res = await fetch(url);
+    const { data } = await res.json();
 
-    const gifs = data.map(img => {
+    const gifs = data.map((img) => {
         return {
             id: img.id,
             title: img.title,
-            url: img.images?.downsized_medium.url
-        }
-    })
+            url: img.images?.downsized_medium.url,
+        };
+    });
 
     return gifs;
-}
+};
